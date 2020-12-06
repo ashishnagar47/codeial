c43a30847c26c9155b4b3411376283847c67c886
ashishnagar47 <nagar123ashish@gmail.com>
2020-12-06 21:49:14 +0530

    first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,43 @@
+const Post = require('../models/post');
+
+const User = require('../models/user');
+
+module.exports.home= async function(req,res){
+
+  try{
+  //populate the user of each post
+    let posts= await Post.find({})
+.populate('user')
+.populate({
+  path:'comments',
+  populate:{
+    path:'user'
+  }
+});
+
+let users=await User.find({});
+
+  return res.render('home',{
+    title:'Codeial | home',
+    posts: posts,
+    all_users:users
+});
+
+  }catch(err){
+    console.log('Error',err);
+    return;
+
+  }
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
+}
+  
+
+// module.exports.actionName = function(req, res){}
