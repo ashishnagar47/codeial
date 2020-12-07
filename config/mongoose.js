bb9a12a213c91bca6efa3a7d1081756a4f01552c
ashishnagar47 <nagar123ashish@gmail.com>
2020-12-07 14:01:17 +0530

    complete

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/codeial_development');
+MONGOURI:"mongodb+srv://Ashish:u0Vz5gqJAg132u1P@cluster0.vhlf5.mongodb.net/Cluster0?retryWrites=true&w=majority";
+mongoose.connect(MONGOURI);
 //mongodb+srv://Ashish:u0Vz5gqJAg132u1P@cluster0.vhlf5.mongodb.net/Cluster0?retryWrites=true&w=majority
 const db = mongoose.connection;
 
